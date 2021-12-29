c42e21d908b84540b1601b468d171e139b60d5c9
henrryforce <luis15ago98@gmail.com>
2021-12-28 22:43:40 -0600

    Lectura correcta de todas las hojas resta generar excel

--- PDF_Log.py
+++ PDF_Log.py
@@ -4,6 +4,8 @@ import os
 import openpyxl
 from PyQt5.QtWidgets import QFileDialog
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def separaMultiplos(self,a):
+        return a.split(sep=' x ')
     def generaReportes(self):
         print('none now')
     def generarExcel(self):
@@ -30,7 +32,83 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             aux=contHojaActiva[i].split(sep=' \n')
             cont2.append(aux[0])
         contHojaActiva = cont2
-        print(cont2)  
+        cont2=[]
+        for i in range(0,len(contHojaActiva)):
+            aux=contHojaActiva[i].split(sep=' =')
+            cont2.append(aux[0])
+        contHojaActiva = cont2
+        #Se optienen las multiplicaciones y resultados de la lista limpia de basura
+        mult =[]
+        resM = []
+        for i in range(2,180,3):
+            mult.append(contHojaActiva[i])
+        for i in range(3,181,3):
+            resM.append(contHojaActiva[i])
+        #Se preparan 2 listas para las multiplicaciones y resultados
+        multip = []
+        auxlistaTab =[]
+        multipR=[]
+        auxlistaTabR =[]
+        for i in range(0,61):
+            if(i<10):
+                auxlistaTab.append(mult[i])
+                auxlistaTabR.append(resM[i])
+            elif(i==10):
+                multip.append(auxlistaTab)
+                auxlistaTab =[]
+                multipR.append(auxlistaTabR)
+                auxlistaTabR =[]
+            if(i<20 and i>=10):
+                auxlistaTab.append(mult[i])
+                auxlistaTabR.append(resM[i])
+            elif(i==20):
+                multip.append(auxlistaTab)
+                auxlistaTab =[]
+                multipR.append(auxlistaTabR)
+                auxlistaTabR =[]
+            if(i<30 and i>=20):
+                auxlistaTab.append(mult[i])
+                auxlistaTabR.append(resM[i])
+            elif(i==30):
+                multip.append(auxlistaTab)
+                auxlistaTab =[]
+                multipR.append(auxlistaTabR)
+                auxlistaTabR =[]
+            if(i<40 and i >=30):
+                auxlistaTab.append(mult[i])
+                auxlistaTabR.append(resM[i])
+            elif(i==40):
+                multip.append(auxlistaTab)
+                auxlistaTab =[]
+                multipR.append(auxlistaTabR)
+                auxlistaTabR =[]
+            if(i<50 and i >=40):
+                auxlistaTab.append(mult[i])
+                auxlistaTabR.append(resM[i])
+            elif(i==50):
+                multip.append(auxlistaTab)
+                auxlistaTab =[]
+                multipR.append(auxlistaTabR)
+                auxlistaTabR =[]
+            if(i<60 and i >=50):
+                auxlistaTab.append(mult[i])
+                auxlistaTabR.append(resM[i])
+            elif(i==60):
+                multip.append(auxlistaTab)
+                auxlistaTab =[]
+                multipR.append(auxlistaTabR)
+                auxlistaTabR =[]
+        hojaT =[] #Variable que contendra la lista de listas de las tablas
+        tabla = []
+        for j in range(0,6):
+            for i in range(0,10):
+                m = self.separaMultiplos(multip[j][i])
+                tabla.append(m)
+                tabla[i].append(multipR[j][i])
+            hojaT.append(tabla)
+            tabla = []
+        hojaT.append(contHojaActiva[0])
+        return hojaT
        
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
@@ -44,7 +122,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         numberOfpages=docum.pageCount
         pagina=[]
         for i in range(0,numberOfpages):
-            self.creaListaDePagina(docum.load_page(i))
+            pagina = self.creaListaDePagina(docum.load_page(i))
+            for j in pagina:
+                print(j)
         
 
 if __name__ == "__main__":
